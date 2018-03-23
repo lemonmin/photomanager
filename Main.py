58add795137635054e3d1a2f42236f4201116da4
gayoung.lee <maysheep740@naver.com>
2018-03-23 15:58:46 +0900

    change v0.1

--- Main.py
+++ Main.py
@@ -1,11 +1,13 @@
 import sys, os
+from pathlib import Path
 from PIL import Image
 from PyQt5 import uic, QtWidgets, QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from contants import *
+from Contants import *
 
+# Load QT Designer UI Interface
 class Form(QtWidgets.QDialog):
     setResultTextSignal = QtCore.pyqtSignal(str)
     def __init__(self, parent=None):
@@ -15,6 +17,10 @@ class Form(QtWidgets.QDialog):
         self.baseListView.doubleClicked.connect(self.baseItemSelected)
         self.cmpListView.doubleClicked.connect(self.cmpItemSelected)
         self.myui.show()
+
+
+   ############### Slots ##############
+
 
     def baseLoadBtnClicked(self):
         baseDir = QtWidgets.QFileDialog.getExistingDirectory(self,"Select base file path","./")
@@ -29,38 +35,44 @@ class Form(QtWidgets.QDialog):
         self.setResultTextSignal.emit(NOTIFY_LOAD_SUCCESS)
 
     def cmpBtnClicked(self):
+        # TBD : 너무 느림... 빠르게 하는 방법 강구 필요.
+        # TBD : Thread로 돌려야할듯.
         self.sameListCmp = {}
         self.sameListBase = {}
         basePath = self.basePath.text().strip()
         cmpPath = self.comparePath.text().strip()
-        # 현재 list에 있는 모든 값을 rowIndex:value dictionary로 가져옴
-        baseFileList = {i:str(self.baseListView.model().item(i).text()) for i in range(self.baseListView.model().rowCount())}
-        cmpFileList = {i:str(self.cmpListView.model().item(i).text()) for i in range(self.cmpListView.model().rowCount())}
-        for bi in baseFileList:
-            try:
-                baseImg = Image.open(basePath+'/'+baseFileList[bi].strip())
-                for ci in cmpFileList:
-                    if basePath != cmpPath:
-                        try:
-                            cmpImg = Image.open(cmpPath+'/'+cmpFileList[ci].strip())
-                            if baseImg == cmpImg:
-                                print("same ",basePath+'/'+baseFileList[bi].strip(), cmpPath+'/'+cmpFileList[ci].strip())
-                                self.sameListCmp[ci] = cmpPath+'/'+cmpFileList[ci].strip()
-                                self.sameListBase[bi] = basePath+'/'+baseFileList[bi].strip()
-                        except Exception as e:
-                            #print('*** Caught exception: %s: %s' % (e.__class__, e))
-                            continue
-            except Exception as e:
-                #print('*** Caught exception: %s: %s' % (e.__class__, e))
-                continue
-        self.setResultTextSignal.emit(NOTIFY_COMPARE_SUCCESS)
-        for i in self.sameListCmp.keys():
-            # 같은 파일들 이름은 붉은 글씨로 표기함
-            self.cmpListView.model().setData(self.cmpListView.model().index(i,0), QBrush(Qt.red), QPalette.Base)
-            # 같은 파일들은 선택해줌
-            self.cmpListView.setSelection(self.cmpListView.rectForIndex(self.cmpListView.model().index(i,0)), QItemSelectionModel.Select)
-        for i in self.sameListBase.keys():
-            self.baseListView.model().setData(self.baseListView.model().index(i,0), QBrush(Qt.red), QPalette.Base)
+        if basePath != '' and cmpPath != '':
+            # 현재 list에 있는 모든 값을 rowIndex:value dictionary로 가져옴
+            baseFileList = {i:str(self.baseListView.model().item(i).text()) for i in range(self.baseListView.model().rowCount())}
+            cmpFileList = {i:str(self.cmpListView.model().item(i).text()) for i in range(self.cmpListView.model().rowCount())}
+            for bi in baseFileList.keys():
+                try:
+                    self.caculateProgress(int(bi)+1)
+                    baseImg = Image.open(basePath + '/' + baseFileList[bi].strip())
+                    for ci in cmpFileList.keys():
+                        if basePath != cmpPath:
+                            try:
+                                cmpImg = Image.open(cmpPath+'/'+cmpFileList[ci].strip())
+                                if baseImg == cmpImg:
+                                    print("same ",basePath+'/'+baseFileList[bi].strip(), cmpPath+'/'+cmpFileList[ci].strip())
+                                    self.sameListCmp[ci] = cmpPath+'/'+cmpFileList[ci].strip()
+                                    self.sameListBase[bi] = basePath+'/'+baseFileList[bi].strip()
+                            except Exception as e:
+                                #print('*** Caught exception: %s: %s' % (e.__class__, e))
+                                continue
+                except Exception as e:
+                    #print('*** Caught exception: %s: %s' % (e.__class__, e))
+                    continue
+            for i in self.sameListCmp.keys():
+                # 같은 파일들 이름은 붉은 글씨로 표기함
+                self.cmpListView.model().setData(self.cmpListView.model().index(i,0), QBrush(Qt.red), QPalette.Base)
+                # 같은 파일들은 선택해줌
+                self.cmpListView.setSelection(self.cmpListView.rectForIndex(self.cmpListView.model().index(i,0)), QItemSelectionModel.Select)
+            for i in self.sameListBase.keys():
+                self.baseListView.model().setData(self.baseListView.model().index(i,0), QBrush(Qt.red), QPalette.Base)
+            self.setResultTextSignal.emit(NOTIFY_COMPARE_SUCCESS)
+        else:
+            self.setResultTextSignal.emit(NOTIFY_SET_PATH)
 
     def cmpSelecAllBtnClicked(self):
         currentText = self.CmpSelecAllBtn.text().strip()
@@ -94,7 +106,7 @@ class Form(QtWidgets.QDialog):
     def baseItemSelected(self):
         # QListView에서는 selectedIndexes()로 현재 선택된 list들을 가져올 수 있다.
         # 그결과 list의 row()로 row 번호, data()로 list 값을 가져올 수 있다.
-        imgPath = str(self.basePath.text().strip() + self.baseListView.selectedIndexes()[0].data().strip()).strip()
+        imgPath = str(self.basePath.text().strip() + self.baseListView.selectedIndexes()[-1].data().strip()).strip()
         #img = Image.open(imgPath)
         # 이미지를 Label에 출력하려면 QPixmap을 사용해야한다.
         imgLabel = QPixmap(imgPath)
@@ -102,7 +114,7 @@ class Form(QtWidgets.QDialog):
         self.baseImgView.setPixmap(imgLabel)
 
     def cmpItemSelected(self):
-        imgPath = str(self.comparePath.text().strip() + self.cmpListView.selectedIndexes()[0].data().strip()).strip()
+        imgPath = str(self.comparePath.text().strip() + self.cmpListView.selectedIndexes()[-1].data().strip()).strip()
         imgLabel = QPixmap(imgPath)
         imgLabel = imgLabel.scaledToHeight(self.cmpImgView.height())
         self.cmpImgView.setPixmap(imgLabel)
@@ -118,6 +130,10 @@ class Form(QtWidgets.QDialog):
                 model.appendRow(item)
             return model
         return None
+
+
+   ############### Functions ##############
+
 
     def loadAllFiles(self, path, files, emptyVol, parent):
         try:
@@ -138,6 +154,12 @@ class Form(QtWidgets.QDialog):
         except Exception as e:
             print('*** Caught exception: %s: %s' % (e.__class__, e))
             self.setResultTextSignal.emit('%s: %s' % (e.__class__, e))
+
+    def caculateProgress(self, index):
+        totalCount = self.baseListView.model().rowCount()
+        progress = int((int(index) / totalCount) * 100)
+        self.progressBar.setValue(progress)
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
